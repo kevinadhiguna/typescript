2a185374ec4f0a840a823ae4f603e38164f947cf
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-04-09 21:01:21 +0700

    refactor: add type of variables

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ console.log("Hello Typescript!");
 var hello = "world";
 // Error : Type 'undefined[]' is not assignable to type 'string'
 // hello = [];
+var hi = "World";
+console.log('Type of "hello" is ', typeof (hello));
+console.log('Type of "hi" is ', typeof (hi));
--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 console.log("Hello Typescript!");
 
-let hello:string = "world";
+let hello: string = "world";
 
 // Error : Type 'undefined[]' is not assignable to type 'string'
 // hello = [];
+
+let hi: String = "World";
+
+console.log('Type of "hello" is ', typeof(hello));
+console.log('Type of "hi" is ', typeof(hi));
