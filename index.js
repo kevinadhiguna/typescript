dffbb673c78579cf1a5844a149a4bb87d56261a2
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-04-09 20:57:56 +0700

    refactor: add typing convention

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 console.log("Hello Typescript!");
+var hello = "world";
+// Error : Type 'undefined[]' is not assignable to type 'string'
+// hello = [];
--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
 console.log("Hello Typescript!");
+
+let hello:string = "world";
+
+// Error : Type 'undefined[]' is not assignable to type 'string'
+// hello = [];
